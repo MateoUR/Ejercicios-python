bc174015de0a28eedd5de8bc6e8c90bef2277f61
MateoUR <mateo.uribe@udea.edu.co>
2024-09-28 10:32:43 -0500

    Update Ejercicios Python - Mateo Uribe Rodriguez y Luisa Fernanda López Cadavid.py

--- Ejercicios Python - Mateo Uribe Rodriguez y Luisa Fernanda López Cadavid.py
+++ Ejercicios Python - Mateo Uribe Rodriguez y Luisa Fernanda López Cadavid.py
@@ -15,11 +15,11 @@ def menu():
         print("1. Calcular el promedio de tres números")
         print("2. Calcular número par o impar")
         print("3. Calcular el factorial de un número")
-        print("4. Calcular raíz cuadrada de un número")
+        print("4. Calcular raiz cuadra de un número")
         print("5. Calcular el mínimo común múltiplo (MCM) de dos números")
         print("6. Salir")
 
-        opcion = input("Elige una opción (1-6): ")
+        opcion = input("Elige una opción (1-4): ")
 
         if opcion == "1":
             try:
@@ -30,19 +30,19 @@ def menu():
             except ValueError:
                 print("Por favor, introducir números válidos.")
                 
-        elif opcion == "2":
+        elif opcion=="2":
             try: 
-                num = float(input("Ingrese el número a comprobar: "))
+                num=float(input("Ingrese el numero a comprobar: "))
                 if num % 2 == 0:
-                    print(f"El número {num} es PAR")
+                    print("el numero ",num," es PAR")
                 else:
-                    print(f"El número {num} es IMPAR")
+                    print("el numero ",num," es IMPAR")
             except ValueError:
                 print("Por favor, ingresa un número válido.")
  
         elif opcion == "3":
             try:
-                n = int(input("Por favor introducir un número entero: "))
+                n = int(input("Porfavor introducir un número entero: "))
                 if n < 0:
                     print("El factorial no es compatible con números negativos.")
                 else:
@@ -52,14 +52,15 @@ def menu():
         
         elif opcion == "4":
             try:
-                num = float(input("Ingrese el número al que desea calcular la raíz cuadrada: "))
-                if num < 0:
-                    print("La raíz cuadrada no es compatible con números negativos.")
+                num = float(input("Ingrese el numero al que desea calcular la raiz cuadrada: "))
+                raiz = math.sqrt(num)
+                if n < 0:
+                    print("La raiz cuadrada no es compatible con números negativos.")
                 else: 
-                    raiz = math.sqrt(num)
-                    print(f"La raíz cuadrada de {num} es: {raiz}")  
+                    print(f"La raiz cuadrada de {n} es:")  
             except ValueError:
-                print("Por favor, introducir un número válido.")
+                    print("Por favor, introducir un número válido.")
+        
         
         elif opcion == "5":
             try:
@@ -70,11 +71,12 @@ def menu():
                 print("Por favor, introduce números válidos.")
         
         elif opcion == "6":
-            print("Saliendo del menú.")
+            print("Saliendo del menu.")
             break
         
         else:
-            print("Opción no válida. Por favor, elige entre 1 y 6.")
+            print("Opción no válida. Por favor, elige entre 1 y 4.")
 
 # Llamada al menú
-menu()o, factorial, mcm
+menu()
+o, factorial, mcm
